06972a31a986817edbc3b3ec9f5f5f3b36098d73
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-09-24 14:17:00 +0300

    Initial commit

---
+++ test/models/dummy_model_test.rb
@@ -0,0 +1,8 @@
+class DummyModelTest < ActiveSupport::TestCase
+  before do
+    puts "before in test"
+  end
+
+  it "does some stupid thing" do
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+
+  def setup
+    puts 'setup in helper'
+  end
+
+  before do
+    puts 'before in helper'
+  end
+end
